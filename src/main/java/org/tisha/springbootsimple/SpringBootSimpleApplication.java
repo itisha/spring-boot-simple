5da43e1a85bb091d1aabe567f6a24d9bfc5e6b85
Tsikhon Kuprevich <tkuprevich@exadel.com>
2020-11-12 21:06:20 +0300

    initial commit

--- src/main/java/org/tisha/springbootsimple/SpringBootSimpleApplication.java
+++ src/main/java/org/tisha/springbootsimple/SpringBootSimpleApplication.java
@@ -1,8 +1,10 @@
 package org.tisha.springbootsimple;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootSimpleApplication
 {
@@ -10,6 +12,9 @@ public class SpringBootSimpleApplication
     public static void main( String[] args )
     {
         SpringApplication.run( SpringBootSimpleApplication.class, args );
+        log.info( "===================" );
+        log.info( "Application Started" );
+        log.info( "===================" );
     }
 
 }
