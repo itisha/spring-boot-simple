3020901c346134588e4e5dc6fbb866e3583b89aa
Tsikhon Kuprevich <tkuprevich@exadel.com>
2020-11-12 20:41:21 +0300

    initial commit

---
+++ src/test/java/org/tisha/springbootsimple/SpringBootSimpleApplicationTests.java
@@ -0,0 +1,15 @@
+package org.tisha.springbootsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
