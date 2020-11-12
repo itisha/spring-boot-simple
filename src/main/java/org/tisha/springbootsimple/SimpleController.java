bb51d2338f65cd473c4ce548bcca89626e7e8219
Tsikhon Kuprevich <tkuprevich@exadel.com>
2020-11-12 21:07:30 +0300

    initial commit

--- src/main/java/org/tisha/springbootsimple/SimpleController.java
+++ src/main/java/org/tisha/springbootsimple/SimpleController.java
@@ -1,10 +1,12 @@
 package org.tisha.springbootsimple;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping( "/" )
 public class SimpleController
@@ -13,8 +15,10 @@ public class SimpleController
     private String message;
 
     @GetMapping
-    public String getSubscription()
+    public String getMessage()
     {
+        log.info( "GetMessage API method Called" );
+        log.info( "message = {}", message );
         return message + " Ok.";
     }
 }
